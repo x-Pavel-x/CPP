84c609a34e4a9a6a56c4e0541fa2b75b78aab0f6
x-Pavel-x <48610903+x-Pavel-x@users.noreply.github.com>
2019-03-25 02:29:30 +0300

    Add files via upload

---
+++ ierarhia_classov.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.h"
+#include <iostream>
+class shifrovanie 
+{
+private:
+	//...
+public:
+	shifrovanie() {};
+	virtual ~shifrovanie() {};
+	//...
+	void method_shifrovaniya() {}
+};
+class asimmetr_shifrovanie : public shifrovanie 
+{
+private:
+	double close_key;
+public:
+	double open_key;
+	asimmetr_shifrovanie() {}
+	~asimmetr_shifrovanie() {}
+};
+class simmetr_shifrovanie : public shifrovanie
+{
+public:
+	simmetr_shifrovanie() {}
+	~simmetr_shifrovanie() {}
+
+};
+class shifr_cesar : public simmetr_shifrovanie 
+{
+private:
+	//...
+public:
+	//...
+	shifr_cesar() {}
+	~shifr_cesar() {}
+};
+int main()
+{
+	return 0;
+}
+
