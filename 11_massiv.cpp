18e9213f0c9adc075b5e6b1eb752689503666749
x-Pavel-x <48610903+x-Pavel-x@users.noreply.github.com>
2019-03-27 15:19:13 +0300

    Add files via upload

---
+++ 11_massiv.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	int* arr;
+	int size = 10, j;
+	 arr =(int*) malloc (size*sizeof(int));
+	 for (int i = 0; i < size; i++)
+	 {
+		 printf("a[%d] = ", i);
+		 scanf_s("%d", &arr[i]);
+	 }
+	for (int i = 0; i < size - 1; i++)
+	{
+		if (arr[i] == arr[i + 1])
+		{
+			for (j = i; j < size - 1; j++)
+			{
+				swap(arr[j], arr[j + 1]);
+			}
+			size--;
+			i--;
+		}
+	}
+	arr = (int*)realloc(arr, size*sizeof(int));
+	for (int i = 0; i < size; i++) { printf(" %d ", arr[i]); }
+	system("pause");
+	return 0;
+}
