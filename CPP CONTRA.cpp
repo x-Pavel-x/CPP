d008a9c24f6999523bc8ff6bfab604e8b550f835
x-Pavel-x <48610903+x-Pavel-x@users.noreply.github.com>
2019-04-29 21:37:31 +0300

    Add files via upload

---
+++ CPP CONTRA.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string st;
+	getline(cin,st);
+	int size = st.length(), j;
+	for (int i = 0; i < size - 1; i++)
+	{
+		if (st[i] == st[i + 1])
+		{
+			for (j = i; j < size - 1; j++)
+			{
+				swap(st[j], st[j + 1]);
+			}
+			size--;
+			i--;
+		}
+	}
+	st.resize(size,' ');
+	cout << st;
+	system("pause");
+	return 0;
+}
